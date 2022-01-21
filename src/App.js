466cbbfc157b55a08b5390f85a8e322a34473800
kais <bahrikais62@gmail.com>
2022-01-21 21:09:26 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Desc from './desc';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    console.log("constructor");
+    super(props)
+  
+    this.state = {
+       name:"roua" ,lastname:"" ,counter:0,
+       show:true
+    }
+  }
+   show= () => {
+     this.setState({show:!this.state.show})
+   };
+   add= () => {
+     this.setState({counter:++this.state.counter})
+   };
+   
+   handlechange= (e) => {
+     this.setState({[e.target.name]:e.target.value})
+   };
+   componentDidMount() {
+    setInterval(()=> this.setState({counter:this.state.counter+1})   , 1000)
+   }
+   componentDidUpdate() {
+     console.log("component will updated");
+   }
+ 
+   
+   
+  render() {
+    console.log("render");
+    return <>
+     {  this.state.show  ? <Desc/> : null} 
+       <button onClick={this.show}> show</button>
+       {this.state.counter}
+       <button onClick={this.add}> +</button>
+       name
+       <input name='name' type="text" onChange={ this.handlechange
+       
+       }/>
+       lastname
+       <input name="lastname" type="text" onChange={ this.handlechange
+       
+       }/>
+       {this.state.name}
+
+
+</> ;
+  }
 }
 
-export default App;
